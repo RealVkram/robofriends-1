ac17b8961eaad7e91519d8e6c8cd0f2dad01abdb
RealVkram <ajayiadegbemi@gmail.com>
2020-04-09 17:33:15 +0100

    adding all middlewares

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,27 @@
-import { CHANGE_SEARCH_FIELD } from "./constant";
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILURE,
+} from "./constant";
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payLoad: text,
 });
+
+export const requestRobots = () => (dispatch) => {
+  dispatch({
+    type: REQUEST_ROBOTS_PENDING
+  })
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => dispatch({
+      type:REQUEST_ROBOTS_SUCCESS,
+      payLoad: data
+    }))
+    .catch(err => dispatch({
+      type: REQUEST_ROBOTS_FAILURE,
+      payLoad: err
+    }))
+}
